4a6fdddd85ed80a6c1dcd93839f8c7e70bc93f01
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-02-23 12:12:10 +0100

    Added cypress test to test URL and portnumber

--- cypress/e2e/checkURL.cy.js
+++ cypress/e2e/checkURL.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("URL test", () => {
+  it("Test if URL is correct", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+    cy.url().should("equals", "http://127.0.0.1:5500/index.html");
+  });
+
+  it("Test if portnumber is correct", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+    cy.url().should("contains", ["5500"]);
+  });
+});
