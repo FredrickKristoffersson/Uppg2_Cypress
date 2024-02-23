a81fda0fe6beb2089a0f27ebe442d6190e11b9a8
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-02-23 12:22:23 +0100

    Changed equals to eq in test for url

--- cypress/e2e/checkURL.cy.js
+++ cypress/e2e/checkURL.cy.js
@@ -1,7 +1,7 @@
 describe("URL test", () => {
   it("Test if URL is correct", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
-    cy.url().should("equals", "http://127.0.0.1:5500/index.html");
+    cy.url().should("eq", "http://127.0.0.1:5500/index.html");
   });
 
   it("Test if portnumber is correct", () => {
